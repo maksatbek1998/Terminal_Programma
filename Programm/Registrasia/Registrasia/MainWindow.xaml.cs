97da5b8aec2166e04fac0227bc368b0a2a7f0b89
maksatbek1998 <maksat.140298.kg@gmail.com>
2020-08-17 10:19:23 +0600

    Maksat

---
+++ Programm/Registrasia/Registrasia/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Registrasia.ClassFolder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Registrasia
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DBconnection dBconnection = new DBconnection();
+        int ID = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ID = dBconnection.DisplayReturn1("SELECT * FROM users WHERE user_login='"+Log.Text.ToString()+"' and user_password='"+Pass.Password.ToString()+"';");
+            if (ID!=0)
+            {
+             AddPeple addPeple = new AddPeple();
+             addPeple.Show();
+             this.Hide();
+            }
+            else
+            {
+                MessParol messParol = new MessParol();
+                messParol.Show();
+            }
+     
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Programm/Registrasia/Registrasia/MessageWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Registrasia.ClassFolder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Registrasia
+{
+    /// <summary>
+    /// Логика взаимодействия для MessageWindow.xaml
+    /// </summary>
+    public partial class MessageWindow : Window
+    {
+        public delegate void MessageDel();
+        public event MessageDel del_;
+        DBconnection dbCon = new DBconnection();
+        public string Id { get; set; }
+        public string TableBasa { get; set; }
+        public MessageWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Id != "" && TableBasa != "")
+                dbCon.RemoveData(TableBasa, Id);
+            del_();
+            this.Close();
+        }
+    }
+}
